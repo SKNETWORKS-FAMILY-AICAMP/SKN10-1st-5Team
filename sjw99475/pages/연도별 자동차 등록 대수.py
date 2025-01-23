f895b54fa0537bd77cf693859ec6061d88fcff7c
123sjw <sjw99475@gmail.com>
2025-01-23 18:38:11 +0900

    자동차 조회 수정정

--- sjw99475/pages/연도별 자동차 등록 대수.py
+++ sjw99475/pages/연도별 자동차 등록 대수.py
@@ -50,7 +50,7 @@ if selected_graph == '선 그래프':
     # sql문의 where 문을 작성함.
     sql_where_region = " or ".join([f"city = '{region}'" for region in selected_regions])
     sql_where_category = " or ".join([f"type = '{category}'" for category in selected_category])
-    sql_where_statement = f"where\n({sql_where_region})\nand ({sql_where_category})\nand (year >= {selected_year[0]} and year <= {selected_year[1]})" if sql_where_region and sql_where_category else "where 1=0"
+    sql_where_statement = f"where 1=1\nand({sql_where_region})\nand ({sql_where_category})\nand (year >= {selected_year[0]} and year <= {selected_year[1]})" if sql_where_region and sql_where_category else "where 1=0"
 
     # 사용자의 선택지대로 sql문 작성
     sql = f'''
@@ -73,17 +73,42 @@ if selected_graph == '선 그래프':
         st.text(', '.join([category for category in selected_category]) + " 차량 검색 결과입니다.")
         st.line_chart(df, x="year", y="value", color="city", height=500)
 
-# elif selected_graph == '지도':
-#     # 연도 선택
-#     selected_year = st.selectbox(
-#         '연도를 선택하세요:',
-#         regions,
-#     )
+elif selected_graph == '지도':
+    # 연도도 선택
+    selected_year = st.slider(
+        '연도를 선택하세요:',
+        2018, 2024)
 
-#     # 자동차 카테고리 선택
-#     selected_category = st.selectbox(
-#         '차량 종류를 선택하세요:',
-#         car_category,
-#     )
+    # 자동차 카테고리 선택
+    selected_category = st.selectbox(
+        '차량 종류를 선택하세요:',
+        car_category,
+        index=None
+    )
 
-    
+    sql_where_year = f"year = '{selected_year}'"
+    sql_where_category = f"type = '{selected_category}'"
+    sql_where_statement = f"where 1=1\nand ({sql_where_category})\nand ({sql_where_year})" if sql_where_category else "where 1=0"
+
+    # 사용자의 선택지대로 sql문 작성
+    sql = f'''
+select
+    year, 
+    city,
+    sum(value) as value
+from car_detail
+{sql_where_statement}
+group by year, city
+order by city
+    ;
+    '''
+    # 서버로부터 결과값을 DataFrame형태로 받음
+    df = sql_execute(db="car", sql=sql)
+
+    if len(df) > 0:
+        df['lon'] = [128.138917, 127.459536, 128.191177, 128.883623, 126.936935, 128.654985, 127.392024, 129.053469, 127.001016, 127.250496, 129.255976, 126.701765, 126.968492, 127.142001, 126.529157, 126.787509, 127.695370]
+        df['lat'] = [37.813732, 37.436038, 35.455151, 36.291491, 35.135247, 35.816177, 36.340544, 35.161954, 37.549449, 36.564806, 35.550875, 37.456783, 34.860440, 35.714971, 33.502377, 36.715894, 36.996171]
+        df['lon'] = df['lon'].astype(float)
+        df['lat'] = df['lat'].astype(float)
+        df["value"] = (df["value"].astype(float)) / 1000
+        st.map(df, latitude="lat", longitude="lon", size="value")
